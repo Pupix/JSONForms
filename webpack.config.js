9bacd93d72afde77a9d112791f7918769b810b46
Pupix <manolea.robert@gmail.com>
2017-08-03 17:08:25 +0200

    Cleanup

--- webpack.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  entry: './lib/json-forms.js',
-  output: {
-    filename: './dist/json-forms.js',
-    sourceMapFilename: './dist/json-forms.map',
-    library: 'JSONForms',
-    libraryTarget: 'umd'
-  }
-};
